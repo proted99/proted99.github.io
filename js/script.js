55aa7421c00e6a10e962cfaa0f7e6e186a4ebd74
Teddy Syach Pratama <tyan30399@gmail.com>
2022-04-23 12:45:01 +0800

    - add Animation AOS

--- js/script.js
+++ js/script.js
@@ -1,11 +1,27 @@
-const nav = document.getElementById("nav");
+const navmenu = document.getElementById("nav-menu");
+const navtab = document.getElementById("nav-tab");
 
 window.addEventListener("scroll", function () {
   scrollposition = window.scrollY;
 
   if (scrollposition >= 60) {
-    nav.classList.add("nav-dark");
+    navmenu.classList.add("nav-dark");
   } else if (scrollposition <= 60) {
-    nav.classList.remove("nav-dark");
+    navmenu.classList.remove("nav-dark");
   }
 });
+
+//klik diluar navbar
+window.addEventListener("click", function (e) {
+  if (e.target != navtab && e.target != navmenu) {
+    navtab.classList.remove("navbar-nav");
+    navmenu.classList.add("hidden");
+  }
+});
+
+//close nav menu after items clicked
+if (window.innerWidth < 1024) {
+  document.querySelectorAll(".navbar-nav li a").forEach((navitem) => {
+    navitem.addEventListener("click", closeNav);
+  });
+}
